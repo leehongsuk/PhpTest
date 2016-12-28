6e22d78eabfb67bdd196ce7a0ec9c1546480e4ba
leehongsuk <lhs0806@nate.com>
2016-12-28 23:17:44 +0900

    `

--- config/DB_CONNECT.php
+++ config/DB_CONNECT.php
@@ -4,12 +4,16 @@ if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
+/*
+ * 디버깅 용도로 사용할것...
+ *
 $query= "CALL  logwrite(?)";
 $stmt = $mysqli->prepare($query);
 
 $stmt->bind_param("s", $PhpSelf);
 $stmt->execute();
 $stmt->close();
+ * */
 
 // $mysqli->set_charset("utf8"); // 한글문제로 설정(my.cfg)파일을 수정하지못할경우
 ?>
