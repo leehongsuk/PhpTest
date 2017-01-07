2d5c17bab7b3a424b726e4bb1d42be00c3df0fd9
이홍석 <lhs0806@nate.com>
2017-01-07 11:42:51 +0900

    영화리스트 페이지처리 조정

--- AjaxJSON/wrk_film_page.php
+++ AjaxJSON/wrk_film_page.php
@@ -2,16 +2,22 @@
 require_once("../config/CONFIG.php");
 require_once("../config/DB_CONNECT.php");
 
-    $post_pageNo     = $_POST["pageNo"] ;
-    $post_pageSize   = $_POST["pageSize"] ;
+    $post_distributor_seq  = $_POST["distributor_seq"] ;
+    $post_film_Nm          = $_POST["film_Nm"] ;
+    $post_pageNo           = $_POST["pageNo"] ;
+    $post_pageSize         = $_POST["pageSize"] ;
 
     $a_json  = array() ;
     $a_page  = array() ;
     $a_list  = array() ;
 
 
-    $query= "CALL SP_WRK_FILM_SEL_COUNT()" ; // <-----
+    $query= "CALL SP_WRK_FILM_SEL_COUNT(?,?)" ; // <-----
     $stmt = $mysqli->prepare($query);
+
+    $stmt->bind_param("is", $post_affiliate
+                          , $post_filmNm
+                          );
     $stmt->execute();
     $stmt->bind_result($count);
     $stmt->fetch();
@@ -20,20 +26,41 @@ require_once("../config/DB_CONNECT.php");
     $pageCount = floor($count / $post_pageSize) + ( ($count % $post_pageSize)>0 ? 1 : 0 ) ;
 
 
-    $query= "CALL SP_WRK_FILM_SEL_PAGE(?,?)" ; // <-----
+    $query= "CALL SP_WRK_FILM_SEL_PAGE(?,?,?,?)" ; // <-----
     $stmt = $mysqli->prepare($query);
 
-    $stmt->bind_param("ii", $post_pageNo, $post_pageSize);
+    $stmt->bind_param("isii", $post_distributor_seq
+                            , $post_film_Nm
+                            , $post_pageNo
+                            , $post_pageSize
+                            );
     $stmt->execute();
 
-    $stmt->bind_result($code,$distributor,$film_nm,$grade,$first_play_dt,$open_dt,$close_dt,$reopem_dt,$reclose_dt,$poster_yn,$images_no,$del_flag);
+    $stmt->bind_result($code
+                      ,$distributor_seq
+                      ,$distributor_nm
+                      ,$distributor_cd
+                      ,$film_nm
+                      ,$grade_nm
+                      ,$first_play_dt
+                      ,$open_dt
+                      ,$close_dt
+                      ,$reopem_dt
+                      ,$reclose_dt
+                      ,$poster_yn
+                      ,$images_no
+                      ,$korabd_gbn_nm
+                      ,$del_flag
+                      );
 
     while ($stmt->fetch())
     {
         array_push($a_list, array("code" => $code
-                                  ,"distributor" => $distributor
+                                  ,"distributor_seq" => $distributor_seq
+                                  ,"distributor_nm" => $distributor_nm
+                                  ,"distributor_cd" => $distributor_cd
                                   ,"film_nm" => $film_nm
-                                  ,"grade" => $grade
+                                  ,"grade_nm" => $grade_nm
                                   ,"first_play_dt" => $first_play_dt
                                   ,"open_dt" => $open_dt
                                   ,"close_dt" => $close_dt
@@ -41,6 +68,7 @@ require_once("../config/DB_CONNECT.php");
                                   ,"reclose_dt" => $reclose_dt
                                   ,"poster_yn" => $poster_yn
                                   ,"images_no" => $images_no
+                                  ,"korabd_gbn_nm" => $korabd_gbn_nm
                                   )
                   );
     }
