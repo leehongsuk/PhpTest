92514e3ac3add00d990fd7ac6dc1d433997ce1e2
이홍석 <lhs0806@nate.com>
2017-01-09 00:29:34 +0900

    시스템로그 보는 화면..

---
+++ AjaxJSON/log_mysql_page.php
@@ -0,0 +1,54 @@
+<?php
+require_once("../config/CONFIG.php");
+require_once("../config/DB_CONNECT.php");
+
+    $post_pageNo       = $_POST["pageNo"] ;
+    $post_pageSize     = $_POST["pageSize"] ;
+
+    $a_json  = array() ;
+    $a_list  = array() ;
+
+
+    $query= "CALL SP_LOG_MYSQL_SEL_COUNT()" ; // <-----
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+
+    $stmt->bind_result($count);
+    $stmt->fetch();
+    $stmt->close();
+
+    $pageCount = floor($count / $post_pageSize) + ( ($count % $post_pageSize)>0 ? 1 : 0 ) ;
+
+
+    $query= "CALL SP_LOG_MYSQL_SEL_PAGE(?,?)" ; // <-----
+    $stmt = $mysqli->prepare($query);
+
+    $stmt->bind_param("ii", $post_pageNo, $post_pageSize);
+    $stmt->execute();
+
+    $stmt->bind_result($event_time
+                      ,$command_type
+                      ,$argument
+                      );
+
+
+    while ($stmt->fetch())
+    {
+        array_push($a_list, array("event_time" => substr($event_time, -8)
+                                  ,"command_type" => $command_type
+                                  ,"argument" => $argument
+                                  )
+                  );
+    }
+
+
+    $a_page = array("pageNo" => $post_pageNo, "pageCount" => $pageCount,"listCount" => $post_pageSize);
+
+    $a_json = array("result" => "ok", "list" => $a_list, "page" => $a_page, "msg" => "");
+
+    $stmt->close();
+
+require_once("../config/DB_DISCONNECT.php");
+
+echo json_encode($a_json,JSON_UNESCAPED_UNICODE);
+?>
---
+++ MainPages/MySql_LOG_View.php
@@ -0,0 +1,87 @@
+<?php require_once("../config/CONFIG.php"); ?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Content-Language" content="ko" />
+
+    <!-- 공통요소 -->
+    <link rel="stylesheet" type="text/css" href="<?=$path_Root?>/js/axisj-1.1.11/ui/arongi/AXJ.css" />
+
+    <script type="text/javascript" src="<?=$path_Root?>/js/axisj-1.1.11/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="<?=$path_Root?>/js/axisj-1.1.11/lib/AXJ.js"></script>
+
+    <!-- 추가하는 UI 요소 -->
+    <link rel="stylesheet" type="text/css" href="<?=$path_Root?>/js/axisj-1.1.11/ui/arongi/AXInput.css" />
+    <link rel="stylesheet" type="text/css" href="<?=$path_Root?>/js/axisj-1.1.11/ui/arongi/AXSelect.css" />
+    <link rel="stylesheet" type="text/css" href="<?=$path_Root?>/js/axisj-1.1.11/ui/arongi/AXGrid.css" />
+    <link rel="stylesheet" type="text/css" href="<?=$path_Root?>/js/axisj-1.1.11/ui/arongi/AXJ.min.css" />
+
+    <script type="text/javascript" src="<?=$path_Root?>/js/axisj-1.1.11/dist/AXJ.min.js"></script>
+    <script type="text/javascript" src="<?=$path_Root?>/js/axisj-1.1.11/lib/AXInput.js"></script>
+    <script type="text/javascript" src="<?=$path_Root?>/js/axisj-1.1.11/lib/AXSelect.js"></script>
+    <script type="text/javascript" src="<?=$path_Root?>/js/axisj-1.1.11/lib/AXGrid.js"></script>
+    <script type="text/javascript" src="<?=$path_Root?>/js/axisj-1.1.11/lib/AXSearch.js"></script>
+
+    <!-- 아이콘사용을 위해서..(http://axicon.axisj.com/) -->
+    <link rel="stylesheet" type="text/css" href="<?=$path_Root?>/js/axicon/axicon.min.css"/>
+
+    <!-- css block -->
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="<?=$path_Root?>/MainCss/Common.css" />
+
+    <script type="text/javascript">
+
+    var gridMySqlLOG = new AXGrid() ; // instance
+
+    var fnObj =
+    {
+        pageStart: function()
+        {
+            gridMySqlLOG.setConfig(
+                    {
+                        targetID : "AXGrid_MySqlLOG",
+                        theme : "AXGrid",
+                        height : 800,
+                        autoChangeGridView: { // autoChangeGridView by browser width
+                            mobile:[0,600], grid:[600]
+                        },
+                        fitToWidth: false, // 너비에 자동 맞춤
+                        colGroup : [
+                            {key:"event_time", label:"시간",     width:"100", align: "center"},
+                            {key:"argument",    label:"실행쿼리", width:"1500"}
+                        ],
+                        body : {
+                            onclick: function(){
+                                location.reload();
+                            }
+                        },
+                        page:{
+                            paging:true,
+                            pageNo:1,
+                            pageSize:100
+                        }
+                    });
+
+            gridMySqlLOG.setList({
+                        ajaxUrl : "<?=$path_AjaxJSON?>/log_mysql_page.php",  // <-----
+                        onLoad  : function(){
+                            //gridTheater.goPageMove(1); // 상대적인 페이지 이동..
+                            //gridTheater.setFocus(3);  // 페이지별 row의 위치 .. 0 부터..
+                        }
+                    });
+        }
+    };
+
+    jQuery(document).ready(fnObj.pageStart.delay(0.1));
+
+    </script>
+</head>
+
+<body>
+
+    <div id="AXGrid_MySqlLOG"></div>
+
+</body>
+</html>
