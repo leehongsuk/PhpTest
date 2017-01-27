f72bc6b4c3257d87f4dce15b942a1166b5e54195
이홍석 <lhs0806@nate.com>
2017-01-27 10:07:11 +0900

    1

--- config/SESSION_IN.php
+++ config/SESSION_IN.php
@@ -2,9 +2,9 @@
     // 이미 로그인 세션이 확보되어 있다면 MainPages쪽으로 간다.....
     if  ($_SESSION['user_seq'])
     {
-        if  ($PhpSelf == $path_Root."/index.php")                     Header("Location:MainPages/"); // 초기 로그인 페이지
+        if  ($PhpSelf == $path_Root."/index.php")                   Header("Location:".$path_Root."MainPages/"); // 초기 로그인 페이지
 
-        if  ($PhpSelf == $path_Root."/MainPages/UserJoin.php")       Header("Location:".$path_Root); // '회원가입' 페이지
+        if  ($PhpSelf == $path_Root."/MainPages/UserJoin.php")      Header("Location:".$path_Root); // '회원가입' 페이지
         if  ($PhpSelf == $path_Root."/MainPages/UserFindIdPw.php")  Header("Location:".$path_Root); // '아이디/비밀번호 찾기' 페이지
     }
 ?>
